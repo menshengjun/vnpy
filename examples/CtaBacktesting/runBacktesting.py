106e9998fb6070d47e5e1e6e18b02edf88ed0e00
zpf <27540337@qq.com>
2018-12-28 20:02:06 +0800

    螺纹钢回测

--- examples/CtaBacktesting/runBacktesting.py
+++ examples/CtaBacktesting/runBacktesting.py
@@ -11,8 +11,9 @@ from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine, MINUTE
 
 
 if __name__ == '__main__':
-    from vnpy.trader.app.ctaStrategy.strategy.strategyKingKeltner import KkStrategy
-    
+    # from vnpy.trader.app.ctaStrategy.strategy.strategyKingKeltner import KkStrategy
+    from vnpy.trader.app.ctaStrategy.strategy.strategy_S002_KK import S002_KK_Strategy
+
     # 创建回测引擎
     engine = BacktestingEngine()
     
@@ -20,20 +21,29 @@ if __name__ == '__main__':
     engine.setBacktestingMode(engine.BAR_MODE)
 
     # 设置回测用的数据起始日期
-    engine.setStartDate('20120101')
+    engine.setStartDate('20180101')
     
+    # # 设置产品相关参数
+    # engine.setSlippage(0.2)     # 股指1跳
+    # engine.setRate(0.3/10000)   # 万0.3
+    # engine.setSize(300)         # 股指合约大小
+    # engine.setPriceTick(0.2)    # 股指最小价格变动
+    #
+    # # 设置使用的历史数据库
+    # engine.setDatabase(MINUTE_DB_NAME, 'IF0000')
+
     # 设置产品相关参数
-    engine.setSlippage(0.2)     # 股指1跳
-    engine.setRate(0.3/10000)   # 万0.3
-    engine.setSize(300)         # 股指合约大小 
-    engine.setPriceTick(0.2)    # 股指最小价格变动
-    
+    engine.setSlippage(1)     # 股指1跳
+    engine.setRate(1/10000)   # 万0.3
+    engine.setSize(10)         # 股指合约大小
+    engine.setPriceTick(1)    # 股指最小价格变动
+
     # 设置使用的历史数据库
-    engine.setDatabase(MINUTE_DB_NAME, 'IF0000')
+    engine.setDatabase(MINUTE_DB_NAME, 'rb0000')
     
     # 在引擎中创建策略对象
     d = {}
-    engine.initStrategy(KkStrategy, d)
+    engine.initStrategy(S002_KK_Strategy, d)
     
     # 开始跑回测
     engine.runBacktesting()
