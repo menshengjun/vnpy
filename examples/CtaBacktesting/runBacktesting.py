89d32c9363d4d937d420e30125019c4db790683a
zpf <27540337@qq.com>
2019-01-25 22:31:14 +0800

    螺纹钢回测

--- examples/CtaBacktesting/runBacktesting.py
+++ examples/CtaBacktesting/runBacktesting.py
@@ -22,14 +22,24 @@ if __name__ == '__main__':
     # 设置回测用的数据起始日期
     engine.setStartDate('20120101')
     
+    # # 设置产品相关参数
+    # engine.setSlippage(0.2)     # 股指1跳
+    # engine.setRate(0.3/10000)   # 万0.3
+    # engine.setSize(300)         # 股指合约大小
+    # engine.setPriceTick(0.2)    # 股指最小价格变动
+    #
+    # # 设置使用的历史数据库
+    # engine.setDatabase(MINUTE_DB_NAME, 'IF0000')
+
     # 设置产品相关参数
-    engine.setSlippage(0.2)     # 股指1跳
-    engine.setRate(0.3/10000)   # 万0.3
-    engine.setSize(300)         # 股指合约大小 
-    engine.setPriceTick(0.2)    # 股指最小价格变动
-    
+    engine.setSlippage(1)     # 股指1跳
+    engine.setRate(1/10000)   # 万0.3
+    engine.setSize(10)         # 股指合约大小
+    engine.setPriceTick(1)    # 股指最小价格变动
+
     # 设置使用的历史数据库
-    engine.setDatabase(MINUTE_DB_NAME, 'IF0000')
+    engine.setDatabase(MINUTE_DB_NAME, 'rb0000')
+
     
     # 在引擎中创建策略对象
     d = {}
